22a0ae0031a746f1fa750abad792302dd2a60162
wssbck <michal@gancarski.pl>
2014-09-08 01:14:49 +0200

    Added uglification to the build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var
 	gulp    = require('gulp'),
 	concat  = require('gulp-concat'),
 	jasmine = require('gulp-jasmine'),
-	jshint  = require('gulp-jshint');
+	jshint  = require('gulp-jshint'),
+	uglify  = require('gulp-uglify');
 
 gulp.task( 'build', function() {
 	gulp.src( 'src/*.js')
 		.pipe( jshint() )
 		.pipe( concat( 'crossfilter-helpers.js' ) )
+		.pipe( gulp.dest( '.' ) )
+		.pipe( uglify() )
+		.pipe( concat( 'crossfilter-helpers.min.js' ) )
 		.pipe( gulp.dest( '.' ) )
 		.pipe( concat( 'index.js' ) )
 		.pipe( gulp.dest( '.' ) );
