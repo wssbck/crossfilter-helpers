aa87d3966a627cceeb07acc6c5318fbeb45c0576
wssbck <michal@gancarski.pl>
2014-09-14 17:12:05 +0200

    Sum and mean reductions, including weighted mean

--- crossfilter-helpers.js
+++ crossfilter-helpers.js
@@ -1,25 +1,86 @@
 var exports = (function(e){
 	'use strict';
 
-	e.count = {
-		add : function(p, v){
-			++p.count;
-			return p;
-		},
-		remove : function(p, v){
-			--p.count;
-			return p;
-		},
-		init : function(){
-			return { count : 0 };
-		},
-		access : function(d) {
-			return d.count;
-		},
-		order : function(p) {
-			return p.count;
+	// reduce by the number of elements of a group
+	e.count = function(){
+		return {
+			add : function(p, v){
+				p.count++;
+				return p;
+			},
+			remove : function(p, v){
+				p.count--;
+				return p;
+			},
+			init : function(){
+				return { count : 0 };
+			},
+			access : function(p){
+				return p.count;
+			}
 		}
 	};
+
+	// reduce by the sum of elements of a group
+	e.sum = function(fn){
+		return {
+			add : function(p, v){
+				p.sum += fn(v);
+				return p;
+			},
+			remove : function(p, v){
+				p.sum -= fn(v);
+				return p;
+			},
+			init : function(){
+				return { sum : 0 };
+			},
+			access : function(p){
+				return p.sum;
+			}
+		}
+	};
+
+	// reduce by the (weighted) mean of elements of a group
+	e.mean = function(fn_f, fn_w){
+		// weight from the parameter, otherwise set it to one, as in non-weighted mean
+		var w = typeof fn_w === 'function' ? fn_w : e.helper_constant(1);
+		return {
+			add : function(p, v){
+				p.count += w(v);
+				p.sum   += w(v) ? fn_f(v) : 0;
+				p.mean   = p.count ? p.sum / p.count : 0;
+				return p;
+			},
+			remove : function(p, v){
+				p.count -= w(v);
+				p.sum   -= w(v) ? fn_f(v) : 0;
+				p.mean   = p.count ? p.sum / p.count : 0;
+				return p;
+			},
+			init : function(){
+				return { count : 0, sum : 0, mean : 0 };
+			},
+			access : function(p){
+				return p.mean;
+			}
+		}
+	};
+
+	return e;
+
+})(exports || {});
+
+module.exports = exports;
+var exports = (function(e){
+	'use strict';
+
+	// build a function returning a selected variable
+	e.helper_constant = function(c) {
+		return function() {
+			return c;
+		}
+	}
 
 	return e;
 
--- src/basic.js
+++ src/basic.js
@@ -1,23 +1,71 @@
 var exports = (function(e){
 	'use strict';
 
-	e.count = {
-		add : function(p, v){
-			++p.count;
-			return p;
-		},
-		remove : function(p, v){
-			--p.count;
-			return p;
-		},
-		init : function(){
-			return { count : 0 };
-		},
-		access : function(d) {
-			return d.count;
-		},
-		order : function(p) {
-			return p.count;
+	// reduce by the number of elements of a group
+	e.count = function(){
+		return {
+			add : function(p, v){
+				p.count++;
+				return p;
+			},
+			remove : function(p, v){
+				p.count--;
+				return p;
+			},
+			init : function(){
+				return { count : 0 };
+			},
+			access : function(p){
+				return p.count;
+			}
+		}
+	};
+
+	// reduce by the sum of elements of a group
+	e.sum = function(fn){
+		return {
+			add : function(p, v){
+				p.sum += fn(v);
+				return p;
+			},
+			remove : function(p, v){
+				p.sum -= fn(v);
+				return p;
+			},
+			init : function(){
+				return { sum : 0 };
+			},
+			access : function(p){
+				return p.sum;
+			}
+		}
+	};
+
+	// reduce by the (weighted) mean of elements of a group
+	// if weight is not set, constant 1 is assumed
+	// if weight equals 0, sum is not changed (current record is ignored)
+	// if count equals 0, mean is zeroed
+	e.mean = function(fn_f, fn_w){
+		var w = typeof fn_w === 'function' ? fn_w : e.helper_constant(1);
+		return {
+			add : function(p, v){
+				p.count += w(v);
+				p.sum   += w(v) ? fn_f(v) : 0;
+				p.mean   = p.count ? p.sum / p.count : 0;
+				return p;
+			},
+			remove : function(p, v){
+				p.count -= w(v);
+				p.sum   -= w(v) ? fn_f(v) : 0;
+				p.mean   = p.count ? p.sum / p.count : 0;
+				return p;
+			},
+			init : function(){
+				return { count : 0, sum : 0, mean : 0 };
+			},
+			access : function(p){
+				return p.mean;
+			}
 		}
 	};
 
