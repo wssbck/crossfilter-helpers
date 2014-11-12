deea10dda16b19e9edea714002a6d5ba589e5d47
wssbck <michal@gancarski.pl>
2014-11-12 21:55:22 +0100

    Added helper accessor function

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,19 @@
 var exports = (function(e){
 	'use strict';
 
+	// build a general data accessor for reduce() parameter functions
+	// if f is a function, it will return this function
+	// otherwise it will build a function that, given a datum d,
+	// will return d[f]
+	e.helper_accessor = function(f) {
+		if (typeof f != 'function') {
+			return function(d) {
+				return d[f];
+			}
+		}
+		return f;
+	}
+
 	// build a function returning a selected variable
 	e.helper_constant = function(c) {
 		return function() {
