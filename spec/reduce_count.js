5d7175a2921c33745c84c35798be68741c864180
wssbck <hi@wssbck.cc>
2015-02-02 22:19:57 +0100

    Simplified dataset for count() test suite

--- spec/reduce_count.js
+++ spec/reduce_count.js
@@ -1,31 +1,31 @@
 var
 	data  = [
-		{ g : "A", f :    10, n :    0 },
-		{ g : "B", f :     4, n :    8 },
-		{ g : "B", f :  null, n :   10 },
-		{ g : "C", f :     3, n : null },
-		{ g : "C", f :     0, n :    4 },
-		{ g : "C", f :  null, n :   15 },
-		{ g : "D", f :  0.15, n :    3 },
-		{ g : "D", f :  0.15, n :    2 },
-		{ g : "D", f :   0.1, n :    2 },
-		{ g : "D", f :   0.2, n : null }
+		{ g : "A" },
+		{ g : "B" },
+		{ g : "B" },
+		{ g : "C" },
+		{ g : "C" },
+		{ g : "C" },
+		{ g : "D" },
+		{ g : "D" },
+		{ g : "D" },
+		{ g : "D" }
 	],
-	cross = require('crossfilter')(data),
-	aggr  = require('../index.js'),
-	dimg  = cross.dimension(function( d ){ return d.g; }),
-    dimf  = cross.dimension(function( d ){ return d.g; });
+	cross   = require('crossfilter')(data),
+	helpers = require('../index.js'),
+	dimg    = cross.dimension(function( d ){ return d.g; }),
+    dimf    = cross.dimension(function( d ){ return d.g; });
 
 describe('Reduce by count', function() {
 
 	var
-		reduceAdd = aggr.count().add,
-		reduceRem = aggr.count().rem,
-		reduceIni = aggr.count().ini,
-		reduceAcc = aggr.count().acc;
+		reduceAdd = helpers.count().add,
+		reduceRem = helpers.count().rem,
+		reduceIni = helpers.count().ini,
+		reduceAcc = helpers.count().acc;
 
 	it('is defined', function() {
-		expect(typeof aggr.count).toBe('function');
+		expect(typeof helpers.count).toBe('function');
 	});
 	
     it('returns reduce add function', function() {
